4d540adbef10997c8b8027b0525defe87b507589
gpuellestorres <gpuellestorres@gmail.com>
2016-02-26 11:10:07 -0300

    Cambio Inicio

--- index.php
+++ index.php
@@ -3,5 +3,33 @@
 ?>
 
 <div class="container">
-	<form></form>
+	<form action="subir.php" method="post" enctype="multipart/form-data">
+		<div id="divNombre" class="form-group">
+			<label for="nombre" class="control-label">Sector 1</label>
+			<div class="checkbox">
+				<input type="checkbox" name="sector" value="1" />
+			</div>
+			<br>
+			<label for="nombre" class="control-label">Sector 2</label>
+			<div class="checkbox">
+				<input type="checkbox" name="sector" value="1" />
+			</div>
+			<br>
+			<label for="nombre" class="control-label">Sector 3</label>
+			<div class="checkbox">
+				<input type="checkbox" name="sector" value="1" />
+			</div>
+			<br>
+			<label for="nombre" class="control-label">Sector 4</label>
+			<div class="checkbox">
+				<input type="checkbox" name="sector" value="1" />
+			</div>
+		</div>
+		<br />
+		<br />
+		<div class="modal-footer">
+			<a href="/admin/ubicaciones" class="btn btn-default btn-lg"><i class="glyphicon glyphicon-remove" aria-hidden="true"></i>&nbsp;Cancelar</a>
+			<button id="botonAgregar" class="btn btn-primary btn-lg"><i class="glyphicon glyphicon-floppy-disk" aria-hidden="true"></i>&nbsp;Agregar</button>
+		</div>
+	</form>
 </div>
