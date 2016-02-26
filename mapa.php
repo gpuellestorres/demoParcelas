c4b47c272868647251d97660cca82a2a9c5cd9ba
gpuellestorres <gpuellestorres@gmail.com>
2016-02-26 11:31:30 -0300

    Arreglo Interfaz Mapa

--- mapa.php
+++ mapa.php
@@ -1,20 +1,25 @@
 <?php 
 	include $_SERVER['DOCUMENT_ROOT']."/header.php";
-
-	if(isset($_POST['sector1']))
-	{
-		echo $_POST["sector1"];
-	}
-	if(isset($_POST['sector2']))
-	{
-		echo "<br>".$_POST["sector2"];
-	}
-	if(isset($_POST['sector3']))
-	{
-		echo "<br>".$_POST["sector3"];
-	}
-	if(isset($_POST['sector4']))
-	{
-		echo "<br>".$_POST["sector4"];
-	}
 ?>
+<div class="container">
+	<div class="col-sm-6 col-sm-offset-3">
+		<?php
+			if(isset($_POST['sector1']))
+			{
+				echo $_POST["sector1"]."<br>";
+			}
+			if(isset($_POST['sector2']))
+			{
+				echo $_POST["sector2"]."<br>";
+			}
+			if(isset($_POST['sector3']))
+			{
+				echo $_POST["sector3"]."<br>";
+			}
+			if(isset($_POST['sector4']))
+			{
+				echo $_POST["sector4"];
+			}
+		?>
+	</div>
+</div>
