2a54dfae327ebb9e7d0ae389da5c7fff7dd39db6
gpuellestorres <gpuellestorres@gmail.com>
2016-02-26 11:28:11 -0300

    Mapa - Isset

--- mapa.php
+++ mapa.php
@@ -1,8 +1,20 @@
 <?php 
 	include $_SERVER['DOCUMENT_ROOT']."/header.php";
 
-	echo $_POST["sector1"];
-	echo "<br>".$_POST["sector2"];
-	echo "<br>".$_POST["sector3"];
-	echo "<br>".$_POST["sector4"];
+	if(isset($_POST['sector1']))
+	{
+		echo $_POST["sector1"];
+	}
+	if(isset($_POST['sector2']))
+	{
+		echo "<br>".$_POST["sector2"];
+	}
+	if(isset($_POST['sector3']))
+	{
+		echo "<br>".$_POST["sector3"];
+	}
+	if(isset($_POST['sector4']))
+	{
+		echo "<br>".$_POST["sector4"];
+	}
 ?>
